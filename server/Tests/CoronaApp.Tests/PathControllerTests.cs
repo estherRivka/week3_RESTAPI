a59cc6454b719dd49eb3cde1cae16535d8266bab
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-05-25 18:19:25 +0300

    update

--- server/Tests/CoronaApp.Tests/PathControllerTests.cs
+++ server/Tests/CoronaApp.Tests/PathControllerTests.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace CoronaApp.Tests
 {
-    public class PathControllerTests
+    public class PathControllerTests : IClassFixture<WebApplicationFactory<Api.Startup>>
     {
+        private readonly WebApplicationFactory<Api.Startup> _factory;
+        public PathControllerTests(WebApplicationFactory<Api.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async void GetPatient_ById_ReturnPatient()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/path/jj");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
     }
 }

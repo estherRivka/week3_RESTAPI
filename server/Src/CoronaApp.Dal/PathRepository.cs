eeecc68624bee8a097e90fd79810e24c99cbd04a
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-06-04 10:39:07 +0300

    updates

--- server/Src/CoronaApp.Dal/PathRepository.cs
+++ server/Src/CoronaApp.Dal/PathRepository.cs
@@ -68,15 +68,7 @@ namespace CoronaApp.Dal
                     newPathsInPropery.Add(pathsInPropery[i]);
             }
 
-            //List<Path> PathsInPropery1 = PathsInPropery.Where(path => info
-            // .GetValue(path) == info1
-            // .GetValue(locationSearch))
-            //    .ToList();
-            //List <Path> PathsInPropery = await _dbcontext.Paths
-            // .Where(path => info
-            //     .GetValue(path) == info
-            //     .GetValue(locationSearch))
-            //      .ToListAsync();
+        
             if (newPathsInPropery == null)
             {
               //  Log.Warning("Couldn't find any paths in property {@pathSearchByCity}", locationSearch.searchByProperty);
@@ -85,56 +77,7 @@ namespace CoronaApp.Dal
             }
             return newPathsInPropery;
         }
-        //public async Task<List<Path>> GetPathsByAge(PathSearch locationSearch)
-        //{
 
-        //    List<Path> paths = await _dbcontext.Paths.Include(p => p.Patient)
-        //        .Where(path => path.Patient.Age == locationSearch.Age)
-        //        .ToListAsync();
-        //    if (paths == null ||)
-        //    {
-        //        Log.Warning("Couldn't find any paths in city {@pathSearchByCity}", locationSearch.City);
-        //        throw new Exception($"NotFound:Couldn't find any paths in {locationSearch.City}");
-
-        //    }
-        //    return paths;
-
-
-        //}
-        //public async Task<List<Path>> GetPathsByCity( PathSearch locationSearch)
-        //{
-        //     List<Path> paths = await _dbcontext.Paths.ToListAsync();
-
-        //    //if (paths == null|| !paths.Any())
-        //    //    return null;
-        //    if (paths == null  )
-        //    {
-        //        Log.Warning("Couldn't find any paths in city {@pathSearchByCity}", locationSearch.City);
-        //        throw new Exception($"NotFound:Couldn't find any paths {locationSearch.City}");
-
-        //    }
-        //    if (!paths.Any())
-        //    {
-        //        return paths;
-
-        //    }
-        //    List <Path> PathsInCity =await _dbcontext.Paths.Where(path => path.City == locationSearch.City).ToListAsync();
-
-        //    //if (PathsInCity == null || !PathsInCity.Any())
-
-        //    //    return null;
-
-        //    if (paths == null )
-        //    {
-        //        Log.Warning("Couldn't find any paths in city {@pathSearchByCity}", locationSearch.City);
-        //        throw new Exception($"NotFound:Couldn't find any paths {locationSearch.City}");
-
-        //    }
-
-        //    return PathsInCity;
-                
-           
-        //}
         public async Task<List<Path>> GetPathsByDate(PathSearch locationSearch)
         {
             List<Path> paths = await _dbcontext.Paths.ToListAsync();
@@ -157,7 +100,7 @@ namespace CoronaApp.Dal
             //    return null;
             if (PathsInDate == null  )
             {
-                Log.Warning("Couldn't find any paths between date {@pathSearchByStartDate} and {@pathSearchByEndDate}", locationSearch.StartDate, locationSearch.EndDate);
+             //   Log.Warning("Couldn't find any paths between date {@pathSearchByStartDate} and {@pathSearchByEndDate}", locationSearch.StartDate, locationSearch.EndDate);
                 throw new Exception($"NotFound:Couldn't find any paths for dates between:{locationSearch.StartDate} till:{locationSearch.EndDate}");
 
             }
@@ -167,60 +110,6 @@ namespace CoronaApp.Dal
 
         }
 
-        //public async Task<List<Path>> GetPathsByStartDate(PathSearch locationSearch)
-        //{
-        //    List<Path> paths = await _dbcontext.Paths.ToListAsync();
 
-        //    //if (!paths.Any())
-        //    //    return null;
-        //    if (paths == null  )
-        //    {
-        //        Log.Warning("Couldn't find any paths !");
-        //        throw new Exception("NotFound:Couldn't find any paths");
-
-        //    }
-        //    if (!paths.Any())
-        //    {
-        //        return paths;
-        //    }
-        //    //throw new Exception("couldnt find any paths!");
-        //    //  DateTime.ParseExact(strDate, "dd/MM/YYYY", CultureInfo.InvariantCulture)
-        //    // DateTime.ParseExact(m.StartDate, "dd/mm/yyyy", null)
-        //    List<Path> PathsInDate =await _dbcontext.Paths.Where(path => path.StartDate > locationSearch.StartDate ).ToListAsync();
-
-        //    if (paths == null || !paths.Any())
-        //    {
-        //        Log.Warning("Couldn't find any paths in city {@pathSearchByCity}", locationSearch.StartDate);
-        //        throw new Exception($"NotFound:Couldn't find any paths for start date: {locationSearch.StartDate}");
-
-        //    }
-        //    // return null;
-        //    return PathsInDate;
-
-
-        //}
-        //public async Task<List<Path>> GetPathsByEndDate(PathSearch locationSearch)
-        //{
-        //    List<Path> paths = await _dbcontext.Paths.ToListAsync();
-
-        //    if (paths==null)
-        //        throw new Exception("couldnt find any paths!");
-        //        if (!paths.Any())
-        //        return paths;
-        //    //  DateTime.ParseExact(strDate, "dd/MM/YYYY", CultureInfo.InvariantCulture)
-        //    List<Path> PathsInDate =await _dbcontext.Paths.Where(path => path.EndDate <locationSearch.EndDate).ToListAsync();
-
-        //    if (PathsInDate == null )
-        //    {
-        //        Log.Warning("Couldn't find any paths in city {@pathSearchByDate}", locationSearch.EndDate);
-
-        //        throw new Exception($"couldnt find any paths for end date: {locationSearch.EndDate}");
-        //    }
-                
-        //       // return null;
-        //    return PathsInDate;
-
-
-        //}
     }
 }

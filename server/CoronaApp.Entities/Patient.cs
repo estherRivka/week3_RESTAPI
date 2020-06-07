9f82d0cd5fcb569f34cd7859cb2b7f9cf126188c
Tzippy Freedman <tzippyfreedman1@gmail.com>
2020-06-07 19:05:22 +0300

    updates

--- server/CoronaApp.Entities/Patient.cs
+++ server/CoronaApp.Entities/Patient.cs
@@ -13,6 +13,8 @@ namespace CoronaApp.Entities
         public List<Path> Paths { get; set; }
         public int Age { get; set; }
         public string UserName { get; set; }
+        public string Name { get; set; }
+
         [JsonIgnore]
         public string Password { get; set; }
         public string Token { get; set; }
